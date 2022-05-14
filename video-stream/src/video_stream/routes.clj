abcc9d2b9440bb942c1181ef66aa1d4d4fba3bdb
Kyle Grierson <griersonky@gmail.com>
2022-05-14 13:03:39 +0100

    video stream from storage

--- video-stream/src/video_stream/routes.clj
+++ video-stream/src/video_stream/routes.clj
@@ -1,38 +1,41 @@
 (ns video_stream.routes
   (:require
-    [aero.core :as aero]
-    [reitit.ring :as ring]
-    [reitit.coercion.malli :as mcoercion]
-    [reitit.ring.coercion :as rrc]
-    [reitit.ring.middleware.parameters :as parameters]
-    [reitit.ring.middleware.muuntaja :as muuntaja]
-    [muuntaja.core :as m]
-    [clj-http.client :as client]
-    [clojure.java.io :as io]))
+   [aero.core :as aero]
+   [reitit.ring :as ring]
+   [reitit.coercion.malli :as mcoercion]
+   [reitit.ring.coercion :as rrc]
+   [reitit.ring.middleware.parameters :as parameters]
+   [reitit.ring.middleware.muuntaja :as muuntaja]
+   [muuntaja.core :as m]
+   [clj-http.client :as client]
+   [clojure.java.io :as io])) 
+
 
 (defn app []
   (ring/ring-handler
-    (ring/router
-      [["/video" {:get {:handler (fn [_]
-                                   (let [host (System/getenv "VIDEO_STORAGE_HOST")
-                                         port (System/getenv "VIDEO_STORAGE_PORT")
-                                         response (client/get (str host ":" port "/video"))]
-                                     {:status  200
-                                      :headers {"Content-Type" "video/mp4"}
-                                      :body    (io/input-stream (.toByteArray response))}))}}]]
-      {:data {:coercion   mcoercion/coercion
-              :muuntaja   m/instance
-              :middleware [parameters/parameters-middleware
-                           muuntaja/format-negotiate-middleware
-                           muuntaja/format-request-middleware
-                           muuntaja/format-response-middleware
-                           rrc/coerce-request-middleware
-                           rrc/coerce-response-middleware]}})))
+   (ring/router
+    [["/video" {:get {:handler (fn [_]
+                                 (let [host (System/getenv "VIDEO_STORAGE_HOST")
+                                       port (System/getenv "VIDEO_STORAGE_PORT")
+                                       response (client/get
+                                                 (str "http://" host ":" port "/video")
+                                                 {:as :stream})]
+                                   {:status  200
+                                    :headers {"Content-Type" "video/mp4"}
+                                    :body    (io/input-stream (:body response))}))}}]
+     {:data       {:coercion mcoercion/coercion}
+      :muuntaja   m/instance
+      :middleware [parameters/parameters-middleware
+                   muuntaja/format-negotiate-middleware
+                   muuntaja/format-request-middleware
+                   muuntaja/format-response-middleware
+                   rrc/coerce-request-middleware
+                   rrc/coerce-response-middleware]}])))
 
 (comment
   (do
     (require
-      '[juxt.clip.repl :refer [start stop set-init! system]])
+     '[juxt.clip.repl :refer [start stop set-init! system]])
     (def system-config (aero/read-config (io/resource "config.edn")))
     (set-init! (constantly system-config))
     (start))
@@ -42,3 +45,5 @@
     (start))
 
   (+ 1 1))
+
+
