a73c2937391c94c0f03133cdb6e68a20484315b0
Kyle Grierson <griersonky@gmail.com>
2022-06-13 20:18:36 +0100

    restart

--- history/src/history/routes.clj
+++ history/src/history/routes.clj
@@ -36,7 +36,7 @@
   (.basicConsume channel queue cbfn ecbfn))
 
 (defn app []
-  (let [_conn "hello-world"]
+  (let [_conn (connect "hello-world")]
     (ring/ring-handler
      (ring/router
       [["/health" {:get {:handler (fn [_]
